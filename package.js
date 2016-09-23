269eb023076c589cd92367771c160c7eb048db53
Jeremy Shimko <jeremy.shimko@gmail.com>
2016-09-23 16:31:49 -0400

    huge rewrite/refactor

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jeremy:react-accounts-ui',
-  version: '0.1.0',
+  version: '1.0.0',
   summary: 'React UI for Meteor Accounts',
   git: 'https://github.com/jshimko/react-accounts-ui',
   documentation: 'README.md'
@@ -29,6 +29,6 @@ Package.onUse(function (api) {
     'softwarerero:accounts-t9n@1.3.3'
   ]);
 
-  api.mainModule('client.js', 'client');
-  api.mainModule('server.js', 'server');
+  api.mainModule('lib/client.js', 'client');
+  api.mainModule('lib/server.js', 'server');
 });
